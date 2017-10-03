5373c6463932ccea56a96fc90cb21d05745d5072
cg-cnu <sreenivas9alapati@gmail.com>
2017-10-03 09:08:08 +0530

    refactor: created multiple functions

--- src/superNewFile.ts
+++ src/superNewFile.ts
@@ -1,7 +1,48 @@
 'use strict';
 import * as vscode from 'vscode';
+import {lstatSync} from 'fs';
 import { dirname, join, normalize } from 'path';
 import * as mkdfp from 'node-mkdirfilep';
+
+
+const trimSpecialChars = (inputPath: string) => {
+    // remove special characters from the selected path
+    const specialChars = ["'", '"', '(', ')', '[', ']', '*', '+', '!', '@', '#', '%', '^', '&', '=', '<', '>', '?', '`']
+    specialChars.forEach(char => inputPath = inputPath.replace(char, ""))
+    return inputPath
+}
+
+const expandVscodeVariables = (text: string) => {
+    // ${workspaceRoot}
+    text.replace('${workspaceRoot}', '')
+    return text;
+}
+
+const expandPath = (inputPath: string) => {
+
+    let finalPath:string;
+    // get final path
+    if (inputPath.startsWith('./') || inputPath.startsWith('../')) {
+        // if the selected path starts with . or ..; it as relative path
+        const currentFilePath: string = dirname(vscode.window.activeTextEditor.document.uri.fsPath);
+        if (currentFilePath === '.') {
+            vscode.window.showWarningMessage("File not saved. Can't create relative path.");
+            return;
+        }
+        finalPath = normalize(join(currentFilePath, inputPath));
+
+    } else if (inputPath.startsWith('~/')) {
+        // if the selected path start with ~; resolve it cross platform way
+        inputPath = inputPath.replace('~/', '')
+        let homedir = process.env[(process.platform == 'win32') ? 'USERPROFILE' : 'HOME'];
+        finalPath = normalize(join(homedir, inputPath));
+
+    } else {
+        // else assume that the selected string is a full path;
+        finalPath = normalize(inputPath);
+    }
+    return finalPath;
+}
 
 export function activate(context: vscode.ExtensionContext) {
 
@@ -10,36 +51,41 @@ export function activate(context: vscode.ExtensionContext) {
         // get the active text editor
         const editor: vscode.TextEditor = vscode.window.activeTextEditor;
         // if no active text editor
-        if (!editor){
+        if (!editor) {
             vscode.window.showWarningMessage("No file open.");
             return;
         }
+
         // get the selection 
-        var selectedText: string = editor.document.getText(editor.selection);
-        // if no selected text
-        if (!selectedText) {
-            vscode.window.showWarningMessage("No valid text selected.");
+        var selectedPath: string = editor.document.getText(editor.selection);
+        // if no selcted path
+        if (!selectedPath){
+            vscode.window.showErrorMessage("No selected path");
             return;
         }
 
-        // get the current file
-        const filePath: string = dirname(vscode.window.activeTextEditor.document.uri.fsPath);
-        // remove special characters
-        const specialChars = ["'", '"', '{', '}', '(', ')', '[', ']', '*', '+', '!', '@', '#', '$', '%', '^', '&', '=', '<', '>', '?', '`', '~']
-        specialChars.forEach(char => selectedText = selectedText.replace(char, ""))
-        // combine it with the file path
-        const finalPath: string = normalize(join(filePath, selectedText));
+        // TODO: refactor; looking ugly 
+        selectedPath = trimSpecialChars(selectedPath);
+        selectedPath = expandPath(selectedPath);
+        selectedPath = expandVscodeVariables(selectedPath);
 
         vscode.window.showInputBox({
-            value: finalPath,
+            value: selectedPath,
             ignoreFocusOut: true,
-            prompt: "You want to create this folder ?",
+            prompt: "Create file/folder?",
             // TODO: select only folders to be created ? 
             // get the workspace name and select everything after that ?
             valueSelection: [-1, -1]
         }).then((modifiedPath) => {
             // create the file or folder
-            mkdfp.create(modifiedPath);
+            if (modifiedPath) {
+                    mkdfp.create(modifiedPath);
+                }
+            // TODO: ask to open the file ? Open if its a file ? 
+            // Needed a callback from mkdfp
+            if( lstatSync(modifiedPath).isFile() ){
+                vscode.workspace.openTextDocument(modifiedPath)
+            }
         })
     });
 
